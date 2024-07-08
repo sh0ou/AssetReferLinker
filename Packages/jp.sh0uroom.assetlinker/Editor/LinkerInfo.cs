57a8d93a06a5fa74fab338aa217037c8f35534df
sh0u <47475540+sh0ou@users.noreply.github.com>
2024-07-09 00:52:53 +0900

    Update LinkerInfo.cs

--- Packages/jp.sh0uroom.assetlinker/Editor/LinkerInfo.cs
+++ Packages/jp.sh0uroom.assetlinker/Editor/LinkerInfo.cs
@@ -1,44 +1,83 @@
+using System;
 using UnityEngine;
 
 namespace sh0uRoom.AssetLinker
 {
-    public class LinkerInfo
+    public static class LinkerInfo
     {
-        public readonly string[] assetStoreURLs = {
+        public static readonly string[] assetStoreURLs = {
             "https://assetstore.unity.com/packages/"
         };
-        public readonly string[] boothURLs = {
+        public static readonly string[] boothURLs = {
             "https://booth.pm/ja/items/",
             "https://booth.pm/en/items/",
             "https://booth.pm/ko/items/",
             "https://booth.pm/zh-tw/items/",
             "https://*.booth.pm/items/"
         };
-        public readonly string[] gumroadURLs = {
+        public static readonly string[] gumroadURLs = {
             "https://*.gumroad.com/l/"
         };
-        public readonly string[] githubURLs = {
+        public static readonly string[] githubURLs = {
             "https://github.com/"
         };
-        public readonly string[] vketStoreURLs = {
+        public static readonly string[] vketStoreURLs = {
             "https://store.vket.com/ja/items/",
             "https://store.vket.com/en/items/"
         };
     }
 
-    [System.Serializable]
-    public enum LicenseType
+    public enum Vendor
     {
-        None = 0,
-        AssetStoreEULA = 1, // Asset Store EULA
-        MIT = 2,    // MIT License
-        Apache = 3, // Apache License 2.0
-        GPL = 4,    // General Public License
-        LGPL = 5,   // Lesser GPL
-        BSD = 6,    // BSD License
-        CC = 7,     // Creative Commons
-        VN3 = 8,    // VN3 License
-        UVL = 9,    // Uni-Virtual License
-        Other = 10
+        Unknown,
+        AssetStore,
+        Booth,
+        Gumroad,
+        GitHub,
+        VKetStore
+    }
+
+    public class LinkerData
+    {
+        public string DownloadURL
+        {
+            get => DownloadURL;
+            set
+            {
+                if (CheckURL(value))
+                    LicenseURL = value;
+            }
+        }
+        public string LicenseURL
+        {
+            get => LicenseURL;
+            set
+            {
+                if (CheckURL(value))
+                    LicenseURL = value;
+            }
+        }
+        public Vendor Vendor { get; set; }
+        public bool IsFree { get; set; }
+        public string[] Paths { get; set; }
+
+        public LinkerData(string downloadURL, string licenseURL, Vendor vendor, bool isFree, string[] paths)
+        {
+            DownloadURL = downloadURL;
+            LicenseURL = licenseURL;
+            Vendor = vendor;
+            IsFree = isFree;
+            Paths = paths;
+        }
+
+        public bool CheckURL(string value)
+        {
+            if (string.IsNullOrEmpty(value) || !value.StartsWith("https://"))
+            {
+                throw new ArgumentException("Invalid URL");
+                return false;
+            }
+            return true;
+        }
     }
 }
